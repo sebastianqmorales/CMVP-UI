f039bfc6c4b7382b1fb99ca0640e7f42eeef8392
Sebastian <swebermorales@gmail.com>
2022-11-25 17:52:54 -0800

    fixed outline on comment input

--- frontend/src/lib/components/commentInput.svelte
+++ frontend/src/lib/components/commentInput.svelte
@@ -12,25 +12,24 @@
 <div class="container">
     <form method="post" class="width border-solid border-2 border-gray-600">
         <!-- <input class="border-solid border-2" placeholder="Share your perspective." type="text"/> -->
-        <textarea class="container input" name="comment" rows="7" placeholder="Share your perspective."></textarea> 
-        <div class="container flex justify-between items-center p-1.5">
-            <label><input type="radio" name="radio" value="no" /> Agreeing</label>        
-            <label><input type="radio" name="radio" value="yes" /> Neutral</label>            
-            <label><input type="radio" name="radio" value="no" /> Disagreeing</label>
-            <button class="border-solid border-2 rounded-lg border-green-500 p-1" type="submit">Comment</button>
+        <textarea class="container" name="comment"  placeholder="Share your perspective."></textarea> 
+        <div class="container flex justify-between items-center p-1.5 border-solid border border-gray-500">
+            <label><input type="radio" name="radio" value="no" /> agreeing</label>        
+            <label><input type="radio" name="radio" value="yes" /> neutral</label>            
+            <label><input type="radio" name="radio" value="no" /> disagreeing</label>
+            <button class="border-solid border-2 rounded-lg border-green-500 p-1" type="submit">comment</button>
         </div>                    
     </form>
 </div>
 
 <style>
-    textarea{
-        border: none !important;
-        border-width: 0px !important;
-    }
-    .input{
-        padding: 0.5rem;
-    }
     .width{
         width: 500px;
+    }
+
+    textarea{
+        height: 100px;
+        padding: 0.5rem;
+        outline: none;
     }
 </style>
