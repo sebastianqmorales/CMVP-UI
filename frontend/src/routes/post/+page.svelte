2af87212e8ef968b0822b1ee86cad9be4d40a3c1
Sebastian <swebermorales@gmail.com>
2022-11-27 21:28:49 -0800

    page structure

---
+++ frontend/src/routes/post/+page.svelte
@@ -0,0 +1,22 @@
+<script>
+	import Comments from './comment.svelte';
+	import CommentInput from './commentInput.svelte';
+	let userName = 'Sebastian';
+	let date = '5 days ago';
+	let commentType = 'Agreeing';
+	let commentContent = 'This is a test for passing in data to the component';
+	let upVote = 100;
+	let downVote = 240;
+</script>
+
+<div class="container">
+	<CommentInput />
+</div>
+
+<Comments {userName} {date} {commentType} {commentContent} {upVote} {downVote} />
+
+<style>
+	.container {
+		padding: 3rem;
+	}
+</style>
