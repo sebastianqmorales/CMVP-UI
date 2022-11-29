d1cf9e5784ff26ad298ac9f832e600c609a5f80f
Sebastian <swebermorales@gmail.com>
2022-11-28 21:20:54 -0800

    comment form data handling

--- frontend/src/routes/post/commentInput.svelte
+++ frontend/src/routes/post/commentInput.svelte
@@ -1,39 +1,58 @@
 <script>
-    // The comment input when completed should output a full comment at the bottom of the input,
-    // Then it should clear the input and render the comment 
-    // The form data will be taken and passed into the props of the Comment component
-    // This is where the input for the comment will go
-    
+	// The comment input when completed should output a full comment at the bottom of the input,
+	// Then it should clear the input and render the comment
+	// The form data will be taken and passed into the props of the Comment component
+	// This is where the input for the comment will go
 
-    // Comment input will send data to mongoDB directly or to server api that will then post data to mongodb
+	// Comment input will send data to mongoDB directly or to server api that will then post data to mongodb
 
+	// Get the data from the form
+	// Post it to the server
+
+	function onSubmit(e) {
+		const formData = new FormData(e.target);
+
+		const data = {};
+		for (let field of formData) {
+			const [key, value] = field;
+			data[key] = value;
+		}
+		console.log(data);
+	}
 </script>
 
 <div class="container">
-    <form method="post" class="border-solid border-2 border-gray-400">
-        <!-- <input class="border-solid border-2" placeholder="Share your perspective." type="text"/> -->
-        <textarea class="container" name="comment"  placeholder="Share your perspective."></textarea> 
-        <div class="container flex justify-between items-center px-2 py-1 border-solid border border-gray-200">
-            <label><input type="radio" name="radio" value="no" /> agreeing</label>        
-            <label><input type="radio" name="radio" value="yes" /> neutral</label>            
-            <label><input type="radio" name="radio" value="no" /> disagreeing</label>
-            <button class="border-solid border-2 rounded-lg border-orange-500 px-2" type="submit">comment</button>
-        </div>                    
-    </form>
+	<form
+		on:submit|preventDefault={onSubmit}
+		method="post"
+		class="border-solid border-2 border-gray-400 "
+	>
+		<!-- <input class="border-solid border-2" placeholder="Share your perspective." type="text"/> -->
+		<textarea class="container" name="comment" placeholder="Share your perspective." />
+		<div
+			class="container flex justify-between items-center px-2 py-1 border-solid border border-gray-200"
+		>
+			<label><input type="radio" name="radio" value="agreeing" /> agreeing</label>
+			<label><input type="radio" name="radio" value="neutral" /> neutral</label>
+			<label><input type="radio" name="radio" value="disagreeing" /> disagreeing</label>
+			<button class="border-solid border-2 rounded-lg border-orange-500 px-2" type="submit"
+				>comment</button
+			>
+		</div>
+	</form>
 </div>
 
 <style>
-    form{       
-        width: 500px;
-    }
+	form {
+		width: 500px;
+	}
 
-    textarea{
-        height: 100px;
-        padding: 0.5rem;
-        outline: none;
-    }
+	textarea {
+		height: 100px;
+		padding: 0.5rem;
+		outline: none;
+	}
 
-    label > input{
-            
-    }
+	label > input {
+	}
 </style>
