3351beff245c0e7fd56badbe745dc96c1132d47f
Sebastian <swebermorales@gmail.com>
2022-12-04 21:25:23 -0800

    adding notes

--- frontend/src/routes/post/+page.server.js
+++ frontend/src/routes/post/+page.server.js
@@ -1,19 +1,16 @@
 /** @type {import('./$types').Actions} */
 export const actions = {
 	default: async ({ request }) => {
+		// To Do: User name comes from the user session or something similar
 		// Get form data and post it to the server
 		const formData = await request.formData();
-		const userName = formData.get('userName');
+		// const userName = formData.get('userName');
 		const commentType = formData.get('commentType');
 		const commentContent = formData.get('commentContent');
 		const upVotes = formData.get('upVotes');
 		const downVotes = formData.get('downVotes');
-		console.log(commentContent);
 		fetch('http://localhost:3000/comments', {
-			// Adding method type
 			method: 'POST',
-
-			// Adding body or contents to send
 			body: JSON.stringify({
 				userName: 'Sebastian',
 				commentType,
@@ -21,8 +18,6 @@ export const actions = {
 				upVotes,
 				downVotes
 			}),
-
-			// Adding headers to the request
 			headers: {
 				'Content-type': 'application/json; charset=UTF-8'
 			}
